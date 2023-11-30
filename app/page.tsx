eb4881d2fe2de01cb3410df07075429cc85f4c18
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-11-30 11:05:05 -0800

    Installed tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 import Link from "next/link";
+import Article from "@/components/Article";
 
 export default async function Index() {
   return (
-    <div className="flex flex-col items-center flex-1 w-full gap-20">
+    <div className="flex w-full flex-1 flex-col items-center gap-20">
       <Navbar />
-      Hi mom!
+
+      <Article title="Hello World" content="This is my first article" />
     </div>
   );
   /*;
